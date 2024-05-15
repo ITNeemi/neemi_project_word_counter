318960952241f013ce03a250011cd6c1037a14c5
Mr Naeem <160456738+ITNeemi@users.noreply.github.com>
2024-05-16 01:35:18 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node 
+// This line is called a shabang, it tells the OS to run it with Node js
+    
+//import the 'inquirer'module whhich is a command line interface for Node .js
+
+import inquirer from "inquirer"
+
+const answers:{
+    Sentence: string
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: ",
+    }
+])
+
+const words = answers.Sentence.trim().split(" ")
+// print array of words to the console 
+console.log(words);
+// print the word count of the sentence to consolog
+console.log(`Your sentence word count is ${words.length}`);
